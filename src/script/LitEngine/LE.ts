239603443ae17892675abafdcb14976e15950042
cczxntken <cczxntken@163.com>
2019-06-08 11:33:58 +0800

    fix

--- src/script/LitEngine/LE.ts
+++ src/script/LitEngine/LE.ts
@@ -1,11 +1,11 @@
 
-import HttpNet from "./Net/HttpNet";
-import WebSocketNet from "./Net/WebSocketNet";
 import { Log } from "./Dlog";
 import { Pool } from "./Pool/NodePool";
 import { Asset } from "./Asset/AssetLoader";
 import { LEvent } from "./Event/EventManager";
 import { Audio } from "./Audio/AudioManager";
+import { LENet } from "./Net/HttpNet";
+import { LESocket } from "./Net/WebSocketNet";
 
 export module LE{
     export class AssetLoader extends Asset.AssetLoader {}
@@ -13,4 +13,6 @@ export module LE{
     export class DLog extends Log.DLog {}
     export class NodePool extends Pool.NodePool {}
     export class AudioManager extends Audio.AudioManager{}
+    export class HttpNet extends LENet.HttpNet{}
+    export class WebSocketNet extends LESocket.WebSocketNet{}
 }
